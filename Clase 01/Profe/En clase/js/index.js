300ff13835b9f15746a879969141bd09783afa38
adrianc4 <adrian_c4@hotmail.com>
2018-08-14 20:41:41 -0300

    Add clase 01

---
+++ Clase 01/Profe/En clase/js/index.js
@@ -0,0 +1,142 @@
+console.log('Init app')
+
+// var pato
+
+// console.log('Primer punto', pato)
+
+// pato = 2
+
+// console.log('segundo punto', pato)
+
+// pato = 4
+
+// console.log('Tercero punto', pato)
+
+// pato = "Hola 'Juan'"
+
+// console.log('Cuarto punto', pato)
+
+// Comentarios
+
+/*
+ Hola soy un comentario largo
+ jgvjhgjhhjhg
+*/
+
+// boolean
+
+var planeMode
+
+planeMode = true
+
+planeMode = false
+
+// string
+
+var name = 'Jauncito'
+
+// number
+
+var age = 29
+
+// undefined
+
+var noAssignation = undefined
+
+// null
+
+var nullValue = null
+
+// Valores estructurados
+
+// Array
+
+// var colors = [ 'red', 'blue', 'yellow', false, undefined, null ]
+
+// console.log(colors[2])
+
+// colors[2] = 'green'
+
+// console.log(colors[2])
+
+// Object
+
+// var label = {
+//   color: 'blue',
+//   name: 'Frío'
+// }
+
+// console.log(label.color)
+
+// console.log(label['color'])
+
+// Un Array de objetos
+
+// var labels = [
+//   {
+//     color: 'blue',
+//     name: 'Frío'
+//   },
+//   {
+//     color: 'yellow',
+//     name: 'Tibio'
+//   },
+//   {
+//     color: 'red',
+//     name: 'Caliente'
+//   }
+// ]
+
+// console.log(labels[1])
+
+// var contactPhone = {
+//   firstName: 'Adrián',
+//   lastName: 'Ferré',
+//   age: 29,
+//   address: {
+//     street: 'Cucha cucha',
+//     number: 1234
+//   }
+// }
+
+// console.log(contactPhone.address.street)
+
+// var obj = {}
+
+// var firstName = prompt('Ingresá tu nombre')
+
+// var message = 'Hola ' + firstName + ' como andas?'
+
+// console.log(message)
+
+// Suma
+
+// var number1 = 31
+
+// var number2 = 5.5
+
+// var result = number1 + number2
+
+// console.log(result)
+
+// Resta
+
+// var res = number2 - number1
+
+// console.log(res)
+
+// Multiplicación
+
+// var mult = number1 * number2
+
+// console.log('Multiplicación', mult)
+
+// var div = number1 / number2
+
+// console.log(div)
+
+var age = prompt('Ingresá tu edad')
+
+var parsedAge = parseFloat(age, 10)
+
+console.log(parsedAge - 2)
