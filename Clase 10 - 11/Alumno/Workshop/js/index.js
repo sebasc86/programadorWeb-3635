320837e9d008b87909ab693b6b78ba0cfa761274
sebasc86 <sebascoscia@gmail.com>
2018-09-14 18:15:56 -0300

    Eliminar Usuario

--- Clase 10 - 11/Alumno/Workshop/js/index.js
+++ Clase 10 - 11/Alumno/Workshop/js/index.js
@@ -2,19 +2,88 @@ console.log('init.app')
 
 var KEY_LOCAL = 'students'
 
-inputName = document.getElementById('firstName')
+var studentsList = getLocalList(KEY_LOCAL)
 
-inputName.onblur = validateInput
-
-inputDni = document.getElementById('dni')
-
-inputDni.onblur = validateDni
+var mainListNode = document.getElementById('mainList')
 
 
+inputNameNode = document.getElementById('firstName')
+
+inputNameNode.onblur = validateInput
+
+inputDniNode = document.getElementById('dni')
+
+inputDniNode.onblur = validateDni
+
+var addStudentButtonNode = document.getElementById('addStudentButton')
+
+addStudentButtonNode.onclick = studentAdd
+
+var deleteButtonNode = document.getElementById('deleteStudentButton')
+
+deleteButtonNode.onclick = deleteStudent
 
 
 
+//Elimina Studiante
 
+function deleteStudent() {
+  
+  var deleteDniNode = document.getElementById('deleteDni')
+  inputValueNode = deleteDniNode.value  
+  var valueExist = searchDniStudent(inputValueNode, studentsList)
+  if(valueExist != -1) {
+    studentsList.splice(valueExist, 1)
+    setLocalList(KEY_LOCAL, studentsList)
+    mainListNode.removeChild(mainListNode.childNodes[valueExist])
+  }
+
+  deleteDniNode.value = ''
+  
+}
+
+
+//Agrega Estudiante
+
+function studentAdd() {
+  firstNameValue = inputNameNode.value
+  dniValue = inputDniNode.value
+
+  var student = {
+    firstName: firstNameValue,
+    dni: dniValue
+  }
+
+  studentsList.push(student)
+
+  setLocalList(KEY_LOCAL, studentsList)
+
+  student = createNode(student)
+
+  mainListNode.appendChild(student)
+
+  inputNameNode.value = ''
+  inputDniNode.value = ''
+  addStudentButtonNode.disabled = true
+  inputNameNode.classList.remove('is-valid')
+  inputDniNode.classList.remove('is-valid')
+
+}
+
+//Valida Boton Submit
+
+function validateSubmitButton () {
+  var addStudentButtonNode = document.getElementById('addStudentButton')
+  var inputFields = document.getElementsByClassName('is-valid')
+
+  if (inputFields.length === 2) {
+    addStudentButtonNode.disabled = false
+  } else {
+    addStudentButtonNode.disabled = true
+  }
+}
+
+//Valida Nombre
 
 function validateInput(event) {
   input = event.target
@@ -23,11 +92,17 @@ function validateInput(event) {
   if (!!inputValue) {
     input.classList.add('is-valid')
     input.classList.remove('is-invalid')
+    
   } else {
     input.classList.remove('is-valid')
     input.classList.add('is-invalid')
+    
   }
+  validateSubmitButton ()
 }
+
+
+//Valida DNI
 
 function validateDni(event) {
   var listLocal = getLocalList(KEY_LOCAL)
@@ -42,19 +117,17 @@ function validateDni(event) {
   if (!isNaN(parsedValue) && valueExist === -1 && inputValue > 0) {
     input.classList.add('is-valid')
     input.classList.remove('is-invalid')
+    
   } else {
     input.classList.remove('is-valid')
     input.classList.add('is-invalid')
+    
   }
+  validateSubmitButton ()
 
-  // if (!!inputValue) {
-  //   input.classList.add('is-valid')
-  //   input.classList.remove('is-invalid')
-  // } else {
-  //   input.classList.remove('is-valid')
-  //   input.classList.add('is-invalid')
-  // }
 }
+
+//BuscaStudiante
 
 function searchDniStudent(dni, studentsList) {
   for (var i = 0; i < studentsList.length; i++) {
@@ -66,6 +139,30 @@ function searchDniStudent(dni, studentsList) {
   return -1
 }
 
+//Crea Nodo
+
+function createNode(objStudent) {
+
+  var id = objStudent.dni
+  var fullName = objStudent.firstName + " " + objStudent.lastName
+  var dni = 'DNI: ' + id
+  var email = 'Email: ' + objStudent.email
+
+
+  var liNode = document.createElement('li')
+  liNode.id = id
+  liNode.className = 'list-group-item'
+
+  liNode.innerHTML = '<h1>' + fullName + '</h1>' + '<h3>' + dni + '</h3>' + '<p>' + email + '</p>'
+
+
+  return liNode
+
+}
+
+
+//busca lista
+
 function getLocalList(key) {
   if (typeof key === 'string') {
     var list = localStorage.getItem(key)
@@ -76,4 +173,19 @@ function getLocalList(key) {
     var listParse = JSON.parse(list)
     return listParse;
   }
+}
+
+//Setea lista
+
+function setLocalList(key, arrayList) {
+  if (Array.isArray(arrayList) && typeof key === 'string') {
+    var arrayToJson = JSON.stringify(arrayList)
+    localStorage.setItem(key, arrayToJson)
+  }
+}
+
+//Apendea//Muestra lista Lista
+for (var i = 0; i < studentsList.length; i++) {
+  student = createNode(studentsList[i])
+  mainListNode.appendChild(student)
 }
