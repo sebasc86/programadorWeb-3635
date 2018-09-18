f0b3092ff2f0cabd0fa3b026262ebc90af45776b
adrianc4 <adrian_c4@hotmail.com>
2018-09-18 20:01:32 -0300

    Add clase 11

--- Clase 10 - 11/Profe/Workshop/js/index.js
+++ Clase 10 - 11/Profe/Workshop/js/index.js
@@ -8,6 +8,8 @@ var LOCAL_KEY = 'studentList'
 
 var studentsList = getLocalList(LOCAL_KEY)
 
+console.log(studentsList)
+
 var mainListNode = document.getElementById('mainList')
 
 var student
@@ -34,6 +36,52 @@ var addStudentButtonNode = document.getElementById('addStudentButton')
 
 addStudentButtonNode.onclick = addStudent
 
+// Eliminar estudiante
+
+var deleteDniNode = document.getElementById('deleteDni')
+
+deleteDniNode.oninput = validateDeleteDniField
+
+var deleteStudentButtonNode = document.getElementById('deleteStudentButton')
+
+deleteStudentButtonNode.onclick = deleteStudent
+
+function validateDeleteDniField (event) {
+  var inputNode = event.target
+
+  var index = searchStudentIndexByDni(inputNode.value, studentsList)
+
+  if (index > -1) {
+    // Habilitamos el botón
+    deleteStudentButtonNode.disabled = false
+  } else {
+    // Deshabilitar el botón
+    deleteStudentButtonNode.disabled = true
+  }
+}
+
+function deleteStudent () {
+  var deleteDniValue = deleteDniNode.value
+
+  var index = searchStudentIndexByDni(deleteDniValue, studentsList)
+
+  // Elimino en la fuente de verdad
+  studentsList.splice(index, 1)
+
+  // Piso la lista del localStorage
+  setLocalList(LOCAL_KEY, studentsList)
+
+  // Busco en el dom el nodo y lo elimino de la lista
+  var node = document.getElementById(deleteDniValue)
+
+  mainListNode.removeChild(node)
+
+  deleteDniNode.value = ''
+  deleteStudentButtonNode.disabled = true
+
+  console.log(studentsList)
+}
+
 function addStudent () {
   var firstNameValue = firstNameNode.value
   var dniValue = dniNode.value
@@ -47,15 +95,17 @@ function addStudent () {
 
   setLocalList(LOCAL_KEY, studentsList)
 
-  student = createStudentNode(student)
+  var studentNode = createStudentNode(student)
 
-  mainListNode.appendChild(student)
+  mainListNode.appendChild(studentNode)
 
   firstName.value = ''
   dniNode.value = ''
   addStudentButtonNode.disabled = true
   firstName.classList.remove('is-valid')
   dniNode.classList.remove('is-valid')
+
+  console.log(studentsList)
 }
 
 // Funciones auxiliares
@@ -165,5 +215,39 @@ function getLocalList (key) {
       // Sino existía devuelvo un array vacío
       return []
     }
+  }
+}
+
+// Buscar estudiante con coincidencias parciales
+
+function searchStudentIndexByText (text, studentsList) {
+  var student
+
+  for (var i = 0; i < studentsList.length; i++) {
+    student = studentsList[i]
+    if (
+      includesText(text, student.firstName) ||
+      includesText(text, student.lastName)
+    ) {
+      return i
+    }
+  }
+
+  return -1
+}
+
+function includesText (text, baseText) {
+  // Valido que ambos parámetros sean string
+  if (typeof text === 'string' && typeof baseText === 'string') {
+    // Verifico si el primer parámetro se encuentra dentro del segundo
+    var textUpperCase = text.toUpperCase()
+    var baseTextUpperCase = baseText.toUpperCase()
+    if (baseTextUpperCase.indexOf(textUpperCase) !== -1) {
+      return true
+    } else {
+      return false
+    }
+  } else {
+    return false
   }
 }
