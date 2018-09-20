76c34696c17a205948cdcf6ccbb979f51702bfb6
adrianc4 <adrian_c4@hotmail.com>
2018-09-20 20:51:35 -0300

    Add clase

--- Clase 12/Profe/En clase/js/index.js
+++ Clase 12/Profe/En clase/js/index.js
@@ -34,16 +34,61 @@ console.log('Init app')
 
 // mainListNode.append('<li id="perrito"><h1>Peritos saltando</h1></li>')
 
-var textNode = $('#text')
+// var textNodes = $('input')
 
-textNode.blur(validateField)
+// textNodes.blur(validateField)
 
-function validateField (event) {
-  var inputNode = $(event.target)
-  // Old S
-  // var value = event.target.value
-  // JQuery
-  var value = inputNode.val()
+// function validateField (event) {
+//   var inputNode = $(event.target)
+//   // Old S
+//   // var value = event.target.value
+//   // JQuery
+//   var value = inputNode.val()
 
-  console.log(value)
+//   console.log(value)
+// }
+
+// var buttonNode = $('button')
+
+// buttonNode.click(buttonClick)
+
+// function buttonClick (event) {
+//   var buttonNode = $(this)
+
+//   var textNodes = $('input')
+
+//   for (var i = 0; i < textNodes.length; i++) {
+//     var nodoJs = textNodes[i]
+
+//     console.log($(nodoJs).val())
+//   }
+// }
+
+// var text1Node = $('#text1')
+
+// text1Node.blur(validateRequiredField)
+
+// function validateRequiredField () {
+//   var inputNode = $(this)
+
+//   var value = inputNode.val()
+
+//   if (value) {
+//     // inputNode.classList.add('is-valid')
+//     inputNode.addClass('is-valid')
+//     inputNode.removeClass('is-invalid')
+//   } else {
+//     inputNode.addClass('is-invalid')
+//     inputNode.removeClass('is-valid')
+//   }
+// }
+
+var divNode = $('div')
+
+divNode.click(toggleRed)
+
+function toggleRed () {
+  var divNode = $(this)
+
+  divNode.toggleClass('red')
 }
