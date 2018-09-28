186bcdeb922000f9a14198505893253d683aaf57
Sebastian <sebascoscia@gmail.com>
2018-09-27 21:12:17 -0300

    ejercicios clase 13

---
+++ Clase 13/Alumno/Ejercicio/js/index.js
@@ -0,0 +1,24 @@
+console.log('init.app')
+
+var counter = 0
+
+$(window).keydown(function (event) {
+  var inputNode = $(this)
+
+  switch (event.which) {
+    case 13:
+      counter = 0
+      $('#counter').html(counter)
+      break
+    case 38:
+      counter++
+      $('#counter').html(counter)
+      console.log(counter)
+      break
+    case 40:
+      counter--
+      $('#counter').html(counter)
+      console.log(counter)
+      break
+  }
+})
---
+++ Clase 13/Alumno/Mini ejercicio/js/index.js
@@ -0,0 +1,5 @@
+console.log('init.app')
+
+$('.fa-trash-alt').on('click', function () {
+  $(this).parent().remove()
+})
