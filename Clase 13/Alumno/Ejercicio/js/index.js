4d70b6bb688dbc7a71af3c73976f6fb88cc67c1f
Sebastian <sebascoscia@gmail.com>
2018-10-02 18:53:57 -0300

    ejercicio 13

--- Clase 13/Alumno/Ejercicio/js/index.js
+++ Clase 13/Alumno/Ejercicio/js/index.js
@@ -1,22 +1,24 @@
-console.log('init.app')
+$(document).ready(function () {
+  console.log('init.app')
 
-var counter = 0
+  var counter = 0
 
-$(document).keydown(function (event) {
-  switch (event.which) {
-    case 13:
-      counter = 0
-      $('#counter').html(counter)
-      break
-    case 38:
-      counter++
-      $('#counter').html(counter)
-      console.log(counter)
-      break
-    case 40:
-      counter--
-      $('#counter').html(counter)
-      console.log(counter)
-      break
-  }
+  $(document).keydown(function (event) {
+    switch (event.which) {
+      case 13:
+        counter = 0
+        $('#counter').html(counter)
+        break
+      case 38:
+        counter++
+        $('#counter').html(counter)
+        console.log(counter)
+        break
+      case 40:
+        counter--
+        $('#counter').html(counter)
+        console.log(counter)
+        break
+    }
+  })
 })
