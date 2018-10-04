42cca8d5e0b39a9883b6dc58c4015c1ad5c9795c
adrianc4 <adrian_c4@hotmail.com>
2018-10-04 19:30:56 -0300

    Add Ajax

--- Clase 14/Profe/Desafío/js/index.js
+++ Clase 14/Profe/Desafío/js/index.js
@@ -1,13 +1,19 @@
 $(document).ready(function () {
-  $('button').on('click', removeElementWithAnimation)
+  $('button').on('click', function () {
+    var node = $(this)
+
+    var id = node.attr('id')
+
+    removeElementWithAnimation(id)
+  })
 
   /**
  * removeElementWithAnimation es una función que recibe un evento
  * y elimina el nodo con una animación
- * @param {*} event
+ * @param {id} string
  */
-  function removeElementWithAnimation (event) {
-    $(this).parent().parent().hide(2000, function () {
+  function removeElementWithAnimation (id) {
+    $('#' + id).parent().parent().hide(2000, function () {
       $(this).remove()
     })
   }
--- Clase 14/Profe/Ejercicio/js/index.js
+++ Clase 14/Profe/Ejercicio/js/index.js
@@ -3,14 +3,20 @@ $(document).ready(function () {
   //   $(this).parent().parent().parent().hide(400)
   // })
   $('.btn-primary').click(function () {
-    var buttonParentParent = $(this).parent().parent()
+    var buttonParentParent = $(this).parent().parent().parent()
 
-    var id = buttonParentParent.attr('id')
+    buttonParentParent.hide(300, function () {
+      var node = $(this)
 
-    removeElementWithAnimation(id)
+      node.remove()
+    })
+
+    // var id = buttonParentParent.attr('id')
+
+    // removeElementWithAnimation(id)
   })
 })
 
-function removeElementWithAnimation (id) {
-  $('#' + id)
-}
+// function removeElementWithAnimation (id) {
+//   $('#' + id)
+// }
